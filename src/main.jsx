1c5c4d7ea0495afd467d7a097985f73935c5e7a3
Deng Benjamin <Deng.Benjamin.6@gmail.com>
2023-04-14 11:19:52 -0600

    - deleted children array

--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import Manager from "./pages/manager/Manager.jsx";
-import EditMenu from "./pages/editMenu/EditMenu.jsx";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [
-      {
-        path: "manager",
-        element: <Manager />,
-        children: [
-          {
-            path: "/manager/menu",
-            element: <EditMenu />,
-          },
-        ],
-      },
-    ],
+   
   },
 ]);
 
