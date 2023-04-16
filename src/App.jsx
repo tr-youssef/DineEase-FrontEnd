79a0233c398da7faf66e55f0065a3a726b088d16
Mousumi Saha <mousumisaha927@gmail.com>
2023-04-16 00:43:19 -0600

    Created login page without css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import AppBar from "./components/appBar/AppBar.jsx";
+import login from "./login";
+
+function appLogin() {
+  return (
+    <main className="appLogin">
+      <login/>
+    </main>
+  )
+}
 
 function App() {
   return (
