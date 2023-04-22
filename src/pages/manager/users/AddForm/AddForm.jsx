409ace588133fa5cfc6041794fbda213e697ea75
Deng Benjamin <Deng.Benjamin.6@gmail.com>
2023-04-22 13:22:22 -0600

    - changed css for addForm to have margin top -added drop down menu for selecting

--- src/pages/manager/users/AddForm/AddForm.jsx
+++ src/pages/manager/users/AddForm/AddForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Input, Button, Form } from "antd";
+import { Input, Button, Form, Select } from "antd";
 import { useNavigate, useParams } from "react-router-dom";
 import { ArrowLeftOutlined } from "@ant-design/icons";
 import { callAPI } from "../../../../utils/FetchData.js";
@@ -46,10 +46,10 @@ export function AddForm () {
       lastName: values.lastName,
       email: values.email,
       role: values.role,
-      password: values.role,
-      confirmPassword: values.role,
+      password: values.role.toLowerCase(), 
+      confirmPassword: values.confirmPassword,
     };
-
+  
     if (Object.keys(id).length === 0) {
       callAPI("http://localhost:5001/users/signup", "POST", data, token).then(() => {
         navigate("/manager/users");
@@ -60,23 +60,24 @@ export function AddForm () {
       });
     }
   };
+  
 
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
+
+  const { Option } = Select;
+
   return (
-    <div className="AddEmployeeForm">
-      <Form
-        name="addEmployee"
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <div className="row">
-          <div className="column">
+    <div className="Employee">
+      <Button icon={<ArrowLeftOutlined />} onClick={handleClick} style={{ background: "#f36805", color: "#FFFFFF", fontSize: "16px", float: "Right", width: "100px" }} size={"large"} />
+      <div className="EmployeeForm">
+        <Form name="addEmployee"  fields={fields} style={{ maxWidth: 600, marginTop: "40px" }} initialValues={{ remember: true }} onFinish={onFinish} onFinishFailed={onFinishFailed} autoComplete="off">
+          <div className="EmployeeInputLine">
             <Form.Item
               label="First name of the employee"
               name="firstName"
+              style={{ fontSize: "24px" }}
               rules={[
                 {
                   required: true,
@@ -84,13 +85,12 @@ export function AddForm () {
                 },
               ]}
             >
-              <Input placeholder="Enter the first name of the employee" />
+              <Input className="EmployeeInput" placeholder="Enter the first name of the employee" size="middle" />
             </Form.Item>
-          </div>
-          <div className="column">
             <Form.Item
               label="Last name of the employee"
               name="lastName"
+              style={{ fontSize: "24px" }}
               rules={[
                 {
                   required: true,
@@ -98,15 +98,12 @@ export function AddForm () {
                 },
               ]}
             >
-              <Input placeholder="Enter the last name of the employee" />
+              <Input className="EmployeeInput" placeholder="Enter the last name of the employee" size="middle" />
             </Form.Item>
-          </div>
-        </div>
-        <div className="row">
-          <div className="column">
             <Form.Item
               label="Email of the employee"
               name="email"
+              style={{ fontSize: "24px" }}
               rules={[
                 {
                   required: true,
@@ -114,13 +111,12 @@ export function AddForm () {
                 },
               ]}
             >
-              <Input placeholder="Enter the email of the employee" />
+              <Input className="EmployeeInput" placeholder="Enter the email of the employee" size="middle" />
             </Form.Item>
-          </div>
-          <div className="column">
             <Form.Item
               label="Role of the employee"
               name="role"
+              style={{ fontSize: "24px" }}
               rules={[
                 {
                   required: true,
@@ -128,47 +124,27 @@ export function AddForm () {
                 },
               ]}
             >
-              <Input placeholder="Enter the role of the employee" />
+              <Select
+                className="EmployeeInput"
+                placeholder="Select the role of the employee"
+                size="middle"
+              >
+                <Option value="manager">Manager</Option>
+                <Option value="chef">Shef</Option>
+                <Option value="server">Server</Option>
+                <Option value="receptionist">Receptionist</Option>
+              </Select>
             </Form.Item>
           </div>
-        </div>
-        <div className="row">
-          <div className="column">
-            <Form.Item
-              label="Password of the employee"
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: "The password of the employee is required",
-                },
-              ]}
-            >
-              <Input placeholder="Enter the password of the employee" />
-            </Form.Item>
-          </div>
-          <div className="column">
-            <Form.Item
-              label="Confirm the password of the employee"
-              name="confirmPassword"
-              rules={[
-                {
-                  required: true,
-                  message: "Please confirm the password of the employee",
-                },
-              ]}
-            >
-              <Input placeholder="Re-enter the password of the employee" />
-            </Form.Item>
-          </div>
-        </div>
-        <Button className="formButton" htmlType="submit">
-          {Object.keys(id).length === 0 ? "Create Employee" : "Save change"}
-        </Button>
-      </Form>
+          <Button style={{ background: "#f36805", color: "#FFFFFF", fontSize: "16px", float: "right", marginTop: "35px" }} size={"large"} htmlType="submit">
+            Add Employee
+          </Button>
+        </Form>
+      </div>
     </div>
   );
-};
+}
+
 
 
 export default AddForm;
