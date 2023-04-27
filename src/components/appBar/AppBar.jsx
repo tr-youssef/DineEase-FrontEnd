6d738a609ec138f0446a1da0966412c94b8fe7fa
tr.youssef <tr.youssef@gmail.com>
2023-04-27 14:31:45 -0600

    add second badge

--- src/components/appBar/AppBar.jsx
+++ src/components/appBar/AppBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./AppBar.css";
 import { ClockCircleTwoTone } from "@ant-design/icons";
-import {Badge,Avatar} from "antd"
+import { Badge, Avatar } from "antd";
 import Clock from "../clock/Clock.jsx";
 import Profil from "../profil/profil.jsx";
 import OrderLogo from "../../assets/menu1.png";
@@ -9,12 +9,12 @@ import ServeDish from "../../assets/servingDish.png";
 import { useEffect, useState } from "react";
 import { callAPI } from "../../utils/FetchData";
 
-
 function AppBar() {
   let auth = JSON.parse(localStorage.getItem("user"));
   const avatar = "https://i.pravatar.cc/100";
   const name = auth?.firstName + " " + auth?.lastName;
   const role = auth?.role;
+  const [NumberOfNewClient, setNumberOfNewClient] = useState(0);
   useEffect(() => {
     callAPI(`http://localhost:5001/booked/availableTables`, "GET", "", auth.token).then((res) => {
       const result = res.map((table) => ({
@@ -23,30 +23,28 @@ function AppBar() {
       }));
       setNumberOfNewClient(result.length);
     });
-}, []);
-
-  const [NumberOfNewClient, setNumberOfNewClient] = useState(0);
+  }, []);
 
   return (
     <div className="AppBar">
-     <div className="LogoContainer">
-      <img className="Logo" src="/src/assets/Logo2.png" alt="Logo" />
-    
-         </div>
+      <div className="LogoContainer">
+        <img className="Logo" src="/src/assets/Logo2.png" alt="Logo" />
+      </div>
       <div className="Clock">
         <ClockCircleTwoTone twoToneColor="#f36805" size={50} />
         <Clock />
       </div>
       <div className="NotificationProfile">
-  {role === "server" && (
+        {role === "server" && (
           <div className="Notification">
             <Badge count={NumberOfNewClient}>
-      <Avatar shape="square" size="large" src={OrderLogo} />
-    </Badge>
-      <img className="ServeDishLogo" src={ServeDish} alt="Serve Dish Logo" />
-     
-    </div>
-  )}
+              <Avatar shape="square" size="large" src={OrderLogo} />
+            </Badge>
+            <Badge count={0}>
+              <Avatar shape="square" size="large" src={ServeDish} />
+            </Badge>
+          </div>
+        )}
         <div className="Profil">
           <Profil avatar={avatar} name={name} role={role} />
         </div>
