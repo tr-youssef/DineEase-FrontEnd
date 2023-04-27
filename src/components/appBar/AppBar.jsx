3771c6e7df6ffa41a6532a465119c34a376658a4
Deng Benjamin <Deng.Benjamin.6@gmail.com>
2023-04-27 14:28:54 -0600

    - fixed appBar

--- src/components/appBar/AppBar.jsx
+++ src/components/appBar/AppBar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./AppBar.css";
-import { Dropdown, Space } from "antd";
 import { ClockCircleTwoTone } from "@ant-design/icons";
 import Clock from "../clock/Clock.jsx";
 import Profil from "../profil/Profil.jsx"
@@ -10,16 +9,6 @@ function AppBar() {
   const avatar = "https://i.pravatar.cc/100";
   const name = auth?.firstName + " " + auth?.lastName;
   const role = auth?.role;
-  const items = [
-    {
-      label: "Duplicate",
-      key: "1",
-      onClick: () => handleDuplicate(record.id),
-    },
-  ];
-  const menuProps = {
-    items,
-  };
 
   return (
     <div className="AppBar">
@@ -31,13 +20,7 @@ function AppBar() {
       <div>
         {role === "Server" && <div className="Notification"></div>}
         <div className="Profil">
-          <Dropdown menu={menuProps}>
-            <a onClick={(e) => e.preventDefault()}>
-              <Space>
-                <Profil avatar={avatar} name={name} role={role} />
-              </Space>
-            </a>
-          </Dropdown>
+          <Profil avatar={avatar} name={name} role={role} />
         </div>
       </div>
     </div>
