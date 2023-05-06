d9b39e34ffbf5b15cfe4d03960798029daa95dcf
tr.youssef <tr.youssef@gmail.com>
2023-05-06 16:59:15 -0600

    update vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import AppBar from "./components/appBar/AppBar.jsx";
-import { Analytics } from "@vercel/analytics/react";
+import { inject } from "@vercel/analytics";
 
 function App() {
+  inject();
   return (
     <div className="App">
       <AppBar />
       <Outlet />
-      <Analytics />
     </div>
   );
 }
