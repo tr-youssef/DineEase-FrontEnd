6aff8be2e40a6f62468e93854da12a47a643f495
tr.youssef <tr.youssef@gmail.com>
2023-05-06 16:55:35 -0600

    Add analytics from vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import AppBar from "./components/appBar/AppBar.jsx";
-
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="App">
       <AppBar />
       <Outlet />
+      <Analytics />
     </div>
   );
 }
