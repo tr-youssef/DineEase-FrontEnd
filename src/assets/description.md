09953d6b03356a746dae84baa5927394ea70060b
tr.youssef <tr.youssef@gmail.com>
2023-05-07 03:33:43 -0600

    delete import img

--- src/assets/description.md
+++
@@ -1,1 +0,0 @@
-it contains assets of our project(images)
--- src/pages/login/SignIn.jsx
+++ src/pages/login/SignIn.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Button, message, Form, Input } from "antd";
 import { useNavigate } from "react-router-dom";
 import { callAPI } from "../../utils/FetchData.jsx";
-import loginImage from "../../assets/WebsiteImage.png";
-import logoImage from "../../assets/Logo1.png";
 
 const SignIn = () => {
   const navigate = useNavigate();
@@ -29,11 +27,11 @@ const SignIn = () => {
     <div style={{ display: "flex" }}>
       {contextHolder}
       <div style={{ flex: 1, overflow: "hidden", width: "100%", height: "99vh" }}>
-        <img src={loginImage} style={{}}></img>
+        <img src={"../../assets/WebsiteImage.png"} style={{}}></img>
       </div>
       <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
         <div style={{ flex: 1, display: "flex", justifyContent: "right", position: "relative", marginBottom: "200px" }}>
-          <img src={logoImage} style={{ objectFit: "contain", width: "20%", height: "20%%", top: 0, right: 50, position: "absolute" }}></img>
+          <img src={"../../assets/Logo1.png"} style={{ objectFit: "contain", width: "20%", height: "20%%", top: 0, right: 50, position: "absolute" }}></img>
         </div>
         <div style={{ flex: 3 }}>
           <Form
